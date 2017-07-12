1a5740bd8cc32d655056a756f73002c201157b3b
Dmitry Bobrenko <d.bobrenko@gmail.com>
2017-07-12 03:14:58 +0300

    added some input args checks

--- reinforceflow/core/experience.py
+++ reinforceflow/core/experience.py
@@ -8,9 +8,12 @@ from collections import deque
 
 class ExperienceReplay(object):
     def __init__(self, size, min_size, batch_size):
+        if min_size < batch_size:
+            raise ValueError("Minimum replay size must be higher or equal to batch size (Got: %s)"
+                             % batch_size)
         self.memory = deque(maxlen=size)
-        self.min_size = min_size
         self.batch_size = batch_size
+        self.min_size = min(size - self.batch_size, min_size)
 
     def add(self, value):
         self.memory.append(value)
