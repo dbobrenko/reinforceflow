5fccdf3fa2a2ab01c82d302637efc0996cb70da5
dbobrenko <d.bobrenko@gmail.com>
2017-08-14 03:53:44 +0300

    updated examples

--- examples/dueling_dqn/dqn_cartpole.py
+++ examples/dueling_dqn/dqn_cartpole.py
@@ -10,7 +10,7 @@ except ImportError:
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
     import reinforceflow
 from reinforceflow.agents.dqn import DQNAgent
-from reinforceflow.nets import mlp
+from reinforceflow.nets import dueling_mlp
 from reinforceflow.core.policy import EGreedyPolicy
 from reinforceflow.core.replay import ExperienceReplay, ProportionalReplay
 reinforceflow.set_random_seed(11)
@@ -18,13 +18,14 @@ reinforceflow.set_random_seed(11)
 
 env = 'CartPole-v0'
 steps = 40000
-agent = DQNAgent(env, net_fn=mlp, use_double=True, use_gpu=True)
+agent = DQNAgent(env, net_fn=dueling_mlp, use_double=False, use_gpu=True)
 agent.train(max_steps=steps,
             render=False,
-            optimizer='rms',
-            learning_rate=0.0001,
-            log_dir='/tmp/reinforceflow/double_dqn/%s/rms' % env,
-            target_freq=2000,
-            experience=ExperienceReplay(capacity=20000, batch_size=32, min_size=1024),
-            # experience=ProportionalReplay(capacity=20000, batch_size=32, min_size=1024),
-            policy=EGreedyPolicy(eps_start=1.0, eps_final=0.1, anneal_steps=20000))
+            optimizer='adam',
+            learning_rate=0.00005,
+            target_freq=500,
+            replay=ExperienceReplay(capacity=steps, batch_size=32, min_size=1024),
+            # replay=ProportionalReplay(capacity=20000, batch_size=32, min_size=1024),
+            policy=EGreedyPolicy(eps_start=1.0, eps_final=0.1, anneal_steps=6000),
+            log_freq=500,
+            log_dir='/tmp/reinforceflow/%s/dueling_dqn/uniform' % env)
